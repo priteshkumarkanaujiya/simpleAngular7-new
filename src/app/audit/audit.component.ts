d706d488fba560d939bfe98af64a1dfea2a43cc9
priteshkumarkanaujiya <72062270+priteshkumarkanaujiya@users.noreply.github.com>
2020-10-07 15:52:48 +0530

    Add files via upload

---
+++ src/app/audit/audit.component.ts
@@ -0,0 +1,44 @@
+﻿import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { first } from 'rxjs/operators';
+
+import { User } from '@/_models';
+import { AuditService, AuthenticationService } from '@/_services';
+
+@Component({ templateUrl: 'audit.component.html' })
+export class AuditComponent implements OnInit, OnDestroy {
+    currentUser: User;
+    currentUserSubscription: Subscription;
+    users: User[] = [];
+    p: number = 1;
+    collection: any ;
+
+    constructor(
+        private authenticationService: AuthenticationService,
+        private AuditService: AuditService
+    ) {
+        this.currentUserSubscription = this.authenticationService.currentUser.subscribe(user => {
+            this.currentUser = user;
+        });
+    }
+
+    ngOnInit() {
+        this.loadAuditDetails();
+    }
+
+    ngOnDestroy() {
+        // unsubscribe to ensure no memory leaks
+        this.currentUserSubscription.unsubscribe();
+    }
+
+
+    private loadAuditDetails() {
+        this.AuditService.getAuditDetails().pipe(first()).subscribe(auditDetails => {
+            console.log(auditDetails);
+            this.collection = auditDetails;
+           // this.TotalPages = Number(Math.ceil(this.auditDetails.length / 8));
+
+
+        });
+    }
+}
